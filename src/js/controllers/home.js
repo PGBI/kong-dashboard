e3b30ecfa786a74315c023ad9d3d41e5b33d5945
PGBI <pbarrau@classy.org>
2017-01-05 16:02:15 +0100

    Remove debug output

--- src/js/controllers/home.js
+++ src/js/controllers/home.js
@@ -5,7 +5,6 @@ angular.module('app').controller("HomeController", ["$scope", "$http", function 
         url: 'https://api.github.com/repos/pgbi/kong-dashboard/contributors'
     }).then(function (response) {
         $scope.contributors = response.data.slice(0,5);
-        console.log($scope.contributors);
     });
 
 }]);
